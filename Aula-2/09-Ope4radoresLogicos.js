26ef0c65e6b9d79cb5db9063fa00fb0b03707076
Gabriel <tellogabriel361@gmail.com>
2023-04-29 12:39:11 -0300

    Aula 2

--- Aula-2/09-Ope4radoresLogicos.js
+++ Aula-2/09-Ope4radoresLogicos.js
@@ -1,11 +1,63 @@
 // Operadores de comparacion
 
-const ciudadDestino = "Bogota";
-const ciudadesDisponibles = new Array("Bogota","Lima","Santiago");
+const ciudadDestino = "Santiago";
+const ciudadesDisponibles = ["Bogota","Lima","Santiago","Montevideo"];
 
 
 // Palabra reservada if
 // Evalua una condicion
+console.log(`Verificando pasajes para ${ciudadDestino}`);
 if (ciudadesDisponibles.indexOf(ciudadDestino) >= 0){
-
+    console.log('Pasaje disponibles para ventas');
 }
+else {
+    console.log('Ciudad no disponible para viajar');
+}
+
+const valorPasje = 1000;
+
+if (valorPasje === 1000) {
+    console.log('El pasaje vale 1000');
+}
+
+//Operadores lògicos
+//Y (AND) - 0 (OR) - NO (NOT)
+//AND = && -Se deben cumplir las dos condiciones
+// OR = || -Se deben cumplir una condicion
+// NOT ! - No se cumple
+
+const edadPasajero = 19;
+const estaAcompañado = true;
+
+console.log(`Verificando pasajes para ${ciudadDestino}`);
+if (edadPasajero >= 18 || estaAcompañado){
+    if (ciudadesDisponibles.indexOf(ciudadDestino) > -1 ){
+        console.log('Pasaje disponibles para ventas');
+    }
+    else {
+        console.log('Ciudad no disponible para viajar');
+    }
+}
+
+// Aplicando logica negativa
+console.log(`Verificando pasajes para ${ciudadDestino}`);
+if ((ciudadesDisponibles.indexOf(ciudadDestino)) > -1 (edadPasajero >= 18 || estaAcompañado) ){
+    console.log('Pasaje disponibles para ventas');
+}
+else {
+    console.log('Ciudad no disponible para viajar');
+}
+
+// Aplicando logica negativa
+console.log(`Verificando pasajes para ${ciudadDestino}`);
+if (!(ciudadesDisponibles.indexOf(ciudadDestino) > -1 (edadPasajero >= 18 || estaAcompañado))){
+    console.log('Ciudad no disponible para viajar or pasajero no cumple las reglas');
+}
+else {
+    console.log('Ciuda deisponible para venta');
+}
+
+
+
+
+
